fdd4db5a995f58ac161c09070396326941467b95
laptop <dukejan86@gmail.com>
2018-04-28 11:29:28 +0200

    T/m opdracht 11

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -3,7 +3,12 @@ package app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@ComponentScan
+@Configuration
 public class Application {
     
     @Autowired
@@ -13,8 +18,13 @@ public class Application {
         controller.showView();
     }
     
+    @Bean
+    public View getView() {
+        return new View2();
+    }
+    
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(Controller.class, View1.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
         Application application = context.getBean(Application.class);
         application.start();
     }
--- src/main/java/app/Controller.java
+++ src/main/java/app/Controller.java
@@ -1,7 +1,9 @@
 package app;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Controller {
     @Autowired
     View view;
